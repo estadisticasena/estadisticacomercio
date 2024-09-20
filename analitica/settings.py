3ac6ef98db046cd079d225ada74a07875d20a867
Dayanapipicano <pipicanodayana2005@gmail.com>
2024-09-20 14:10:01 -0500

    ajus

--- analitica/settings.py
+++ analitica/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
    
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://analitica-production.up.railway.app']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
