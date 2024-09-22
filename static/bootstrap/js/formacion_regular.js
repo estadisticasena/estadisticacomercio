936f49635e7bbd785647e509c805e63b8da4b1c8
laravelBasic <pipicanodayana2005@gmail.com>
2024-09-22 17:58:22 -0500

    ajustes

--- static/bootstrap/js/formacion_regular.js
+++ static/bootstrap/js/formacion_regular.js
@@ -3,39 +3,9 @@
     document.querySelector('#submit-second-modal').addEventListener('click', function(event) {
         event.preventDefault(); // Evita el comportamiento por defecto del botón
 
-        // Selecciona el formulario en el segundo modal
-        var form = document.querySelector('#second-modal-form');
-        var formData = new FormData(form);
-
-        // Envía los datos del formulario usando fetch
-        fetch(form.action, {
-            method: 'POST',
-            body: formData,
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-                'X-CSRFToken': form.querySelector('[name="csrfmiddlewaretoken"]').value
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                // Simula el clic en el botón de cerrar para cerrar el segundo modal
-                document.querySelector('#submit-second-modal').click();
-
-               
-             
-            } else {
-                // Muestra errores si la respuesta es fallida
-                console.log('Error:', data.errors);
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+       
+       
     });
-
-
-
 
 });
 
