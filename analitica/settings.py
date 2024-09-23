bf9ac9a5dfb73e272060578d43ba15a7b7ca293b
Dayanapipicano <pipicanodayana2005@gmail.com>
2024-09-23 10:01:14 -0500

    consulta

--- analitica/settings.py
+++ analitica/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-9+%g4dk0x&@^d(gac*gf2p5o-oxbz87as5b$ru&k&wdg@x5f&l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['estadisticacomercio-production.up.railway.app']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['estadisticacomercio-production.up.railway.app']
+#ALLOWED_HOSTS = []
 
 
 import pymysql
@@ -89,28 +89,28 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'analisis',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'localhost',
-        'PORT': '3306',
-    }
-}
-
-
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': os.environ.get('DB_NAME'),
-#        'USER': os.environ.get('DB_USER'),
-#        'PASSWORD': os.environ.get('DB_PASSWORD'),
-#        'HOST': os.environ.get('DB_HOST'),
-#        'PORT': os.environ.get('DB_PORT'),
+#        'NAME': 'analisis',
+#        'USER': 'root',
+#        'PASSWORD': '',
+#        'HOST':'localhost',
+#        'PORT': '3306',
 #    }
 #}
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
 
 
 
